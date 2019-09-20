e733d43c9e733fb51156e1258cd2f4f645c42e51
CiubancanCristian <cristian@ciubancan.com>
2019-09-20 11:19:30 +0300

    added about section

--- src/sections/about/about.section.jsx
+++ src/sections/about/about.section.jsx
@@ -4,7 +4,44 @@ import "./about.styles.css";
 
 const AboutSection = () => (
 	<div className="about-section">
-		<p>AICI VA FI About</p>
+		<h3>
+			IGNITING LEADERS, ILLUMINATING COMMUNITIES, AND INSPIRING LEGACIES.
+		</h3>
+		<p>
+			We are a co-ed business leadership fraternity committed to inspire
+			the best in everyone. We aspire to become visionaries, influencers,
+			and mobilizers. This means we dream big, rally support, and get it
+			done. Our mission is clear – to inspire leaders. As a Partnership,
+			our members are each co-owners of this mission.
+		</p>
+		<p>
+			We are differentiated in that our Partners don’t just want to land a
+			career, we each want to fulfill a calling. We are proud of our
+			history of campus community leaders and founders. During their short
+			time on campus, they each left a legacy because they saw a need to
+			fill, a problem to solve, or someone to care about. We are inspired
+			to carry on the torch with the aim to leave our world better than we
+			found it.
+		</p>
+		<p className="aside-paragraph">Welcome to Lambda Phi Nu, where we</p>
+		<h3>“GROW FORWARD, GIVE BACK.”</h3>
+		<h4>CORE VALUES</h4>
+		<p>
+			Do you follow through with your promises? Have an insatiable desire
+			to keep learning? Naturally inclined to serve others? Enjoy forming
+			relationships? And always seeking to better yourself?
+		</p>
+		<p>
+			If yes, we do too. That’s why we hold on to the values of Integrity,
+			Curiosity, Altruism, Relationships, and Excellence.
+		</p>
+		<div className="core-values">
+			<h5>Integrity</h5>
+			<h5>Curiosity</h5>
+			<h5>Altruism</h5>
+			<h5>Relationships</h5>
+			<h5>Excellence</h5>
+		</div>
 	</div>
 );
 
